b0e67bbcf750035618e800089a3eb17fab1dfeaf
Vinod Mehta <134594051+Vinod-mehta123@users.noreply.github.com>
2024-10-08 21:58:31 +0530

    Add files via upload

---
+++ minimum_number_of_swaps_to_makeString_balances.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+       int open = 0;
+        for(int i=0; i<s.length(); i++){
+           
+            if(s[i] == '[' ){
+               open++;
+            }else if(open > 0){
+               open--;
+            }
+        }
+       return (open+1)/2;
+        
+    }
+};
